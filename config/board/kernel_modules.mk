9f097a426ab4052a63c97e0e76be88e5e1ac6115
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-09-12 10:21:35 +0800

    [ax_dragonite] adding axdragonite module
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/board/kernel_modules.mk
+++ config/board/kernel_modules.mk
@@ -2,6 +2,7 @@ AXION_COMMON_PATH := device/axion/common
 AXION_COMMON_MODULE_ROOT := $(AXION_COMMON_PATH)/kernel/modules
 
 AXION_COMMON_KERNEL_MODULES := \
+    ax_dragonite
 
 ifneq ($(strip $(TARGET_KERNEL_SOURCE)),)
 ifeq ($(strip $(TARGET_PREBUILT_KERNEL)),)
---
+++ kernel/modules/axion_modules.mk
@@ -0,0 +1,1 @@
+axion_dragonite := y
